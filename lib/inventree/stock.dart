0bf02dad64158c132427c5563d997ec5b096bd49
Oliver Walters <oliver.henry.walters@gmail.com>
2020-04-01 10:04:55 +1100

    Add endpoint for StockLocation model

--- lib/inventree/stock.dart
+++ lib/inventree/stock.dart
@@ -20,3 +20,24 @@ class InvenTreeStockItem extends InvenTreeModel {
     return item;
   }
 }
+
+
+class InvenTreeStockLocation extends InvenTreeModel {
+  @override
+  String URL = "stock/location/";
+
+  InvenTreeStockLocation() : super();
+
+  InvenTreeStockLocation.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+
+  }
+
+  @override
+  InvenTreeModel createFromJson(Map<String, dynamic> json) {
+
+    var loc = InvenTreeStockLocation.fromJson(json);
+
+    return loc;
+
+  }
+}
