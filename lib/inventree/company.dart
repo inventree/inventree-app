039b09564e33e137d5109d06ccf7cc565ae917eb
Oliver Walters <oliver.henry.walters@gmail.com>
2020-04-01 16:24:02 +1100

    Add class for representing SupplierPart model

--- lib/inventree/company.dart
+++ lib/inventree/company.dart
@@ -1,6 +1,9 @@
 import 'model.dart';
 
 
+/*
+ * The InvenTreeCompany class repreents the Company model in the InvenTree database.
+ */
 class InvenTreeCompany extends InvenTreeModel {
   @override
   String URL = "company/";
@@ -16,5 +19,27 @@ class InvenTreeCompany extends InvenTreeModel {
     var company = InvenTreeCompany.fromJson(json);
 
     return company;
+  }
+}
+
+
+/*
+ * The InvenTreeSupplierPart class represents the SupplierPart model in the InvenTree database
+ */
+class InvenTreeSupplierPart extends InvenTreeModel {
+  @override
+  String url = "company/part/";
+
+  InvenTreeSupplierPart() : super();
+
+  InvenTreeSupplierPart.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    // TODO
+  }
+
+  @override
+  InvenTreeModel createFromJson(Map<String, dynamic> json) {
+    var part = InvenTreeSupplierPart.fromJson(json);
+
+    return part;
   }
 }
