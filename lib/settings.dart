aa865eea1b5371f416033a489407bc7da9f93725
Oliver Walters <oliver.henry.walters@gmail.com>
2020-04-03 23:03:45 +1100

    Add "About" dialog

--- lib/settings.dart
+++ lib/settings.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'login_settings.dart';
 
+import 'package:package_info/package_info.dart';
+
 class InvenTreeSettingsWidget extends StatefulWidget {
   // InvenTree settings view
 
@@ -26,6 +28,12 @@ class _InvenTreeSettingsState extends State<InvenTreeSettingsWidget> {
                 subtitle: Text("Configure server and login settings"),
                 onTap: _editServerSettings,
             ),
+            Divider(),
+            ListTile(
+              title: Text("About"),
+              subtitle: Text("App details"),
+              onTap: _about,
+            ),
           ],
         )
       )
@@ -34,5 +42,35 @@ class _InvenTreeSettingsState extends State<InvenTreeSettingsWidget> {
 
   void _editServerSettings() {
     Navigator.push(context, MaterialPageRoute(builder: (context) => InvenTreeLoginSettingsWidget()));
+  }
+
+  void _about() async {
+
+    PackageInfo.fromPlatform().then((PackageInfo info) {
+      showDialog(
+        context: context,
+        child: new SimpleDialog(
+            title: new Text("About InvenTree"),
+            children: <Widget>[
+                ListTile(
+                  title: Text("App Name"),
+                  subtitle: Text("${info.appName}"),
+                ),
+                ListTile(
+                  title: Text("App Version"),
+                  subtitle: Text("${info.version}"),
+                ),
+                ListTile(
+                  title: Text("Package Name"),
+                  subtitle: Text("${info.packageName}"),
+                ),
+                ListTile(
+                  title: Text("Build Number"),
+                  subtitle: Text("${info.buildNumber}")
+                ),
+            ]
+        ),
+      );
+    });
   }
 }
