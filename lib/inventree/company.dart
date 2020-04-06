1faf0fc575923ceccf1fe83dce45fbe09fe47fe1
Oliver Walters <oliver.henry.walters@gmail.com>
2020-04-07 00:09:23 +1000

    Data introspection for Company model

--- lib/inventree/company.dart
+++ lib/inventree/company.dart
@@ -12,6 +12,16 @@ class InvenTreeCompany extends InvenTreeModel {
 
   String get image => jsondata['image'] ?? '';
 
+  String get website => jsondata['website'] ?? '';
+
+  String get phone => jsondata['phone'] ?? '';
+
+  String get email => jsondata['email'] ?? '';
+
+  bool get isSupplier => jsondata['is_supplier'] ?? false;
+
+  bool get isCustomer => jsondata['is_customer'] ?? false;
+
   InvenTreeCompany.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
     // TODO
   }
