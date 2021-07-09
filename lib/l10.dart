855a1481b24a2ac55f68a6e7754062ce3b868b41
Oliver <oliver.henry.walters@gmail.com>
2021-07-10 00:19:20 +1000

    I18N fallback for null context

--- lib/l10.dart
+++ lib/l10.dart
@@ -1,9 +1,22 @@
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:one_context/one_context.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
 
+import 'package:one_context/one_context.dart';
+import 'package:flutter/material.dart';
 
 // Shortcut function to reduce boilerplate!
 I18N L10()
 {
-  return I18N.of(OneContext().context)!;
+  BuildContext? _ctx = OneContext().context;
+
+  if (_ctx != null) {
+    I18N? i18n = I18N.of(_ctx);
+
+    if (i18n != null) {
+      return i18n;
+    }
+  }
+
+  // Fallback for "null" context
+   return I18NEn();
 }
