08a29bcbd01eb191e2ae53c142abb49ad40b121c
Guus Geurkink <guus.geurkink@gmail.com>
2021-08-18 15:58:17 +0200

    Added equals operator and some kind of hashcode (not sure if it's correct) so we can get unique models by using .toSet()

--- lib/inventree/model.dart
+++ lib/inventree/model.dart
@@ -9,15 +9,12 @@ import 'package:inventree/widget/dialogs.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:path/path.dart' as path;
-import 'package:http/http.dart' as http;
 
 import '../l10.dart';
 import '../api_form.dart';
 
-
 // Paginated response object
 class InvenTreePageResponse {
-
   InvenTreePageResponse() {
     results = [];
   }
@@ -28,7 +25,7 @@ class InvenTreePageResponse {
 
   // Total number of results in the dataset
   int count = 0;
-  
+
   int get length => results.length;
 
   List<InvenTreeModel> results = [];
@@ -38,7 +35,17 @@ class InvenTreePageResponse {
  * The InvenTreeModel class provides a base-level object
  * for interacting with InvenTree data.
  */
+const int SOME_ID_PRIME = 5393;
+
 class InvenTreeModel {
+  @override
+  bool operator ==(Object other) {
+    return other is InvenTreeModel &&
+        other.hashCode == this.hashCode;
+  }
+
+  @override
+  int get hashCode => this.runtimeType.hashCode + (SOME_ID_PRIME * pk) + (SOME_ID_PRIME * SOME_ID_PRIME * name.hashCode);
 
   // Override the endpoint URL for each subclass
   String get URL => "";
@@ -48,7 +55,6 @@ class InvenTreeModel {
   String WEB_URL = "";
 
   String get webUrl {
-
     if (api.isConnected()) {
       String web = InvenTreeAPI().baseUrl;
 
@@ -59,22 +65,22 @@ class InvenTreeModel {
       web = web.replaceAll("//", "/");
 
       return web;
-
     } else {
       return "";
     }
-
   }
 
   // Fields for editing / creating this model
   // Override per-model
   Map<String, dynamic> formFields() {
-
     return {};
   }
 
-  Future<void> createForm(BuildContext context, String title, {String fileField = "", Map<String, dynamic> fields=const{}, Map<String, dynamic> data=const {}, Function(dynamic)? onSuccess}) async {
-
+  Future<void> createForm(BuildContext context, String title,
+      {String fileField = "",
+      Map<String, dynamic> fields = const {},
+      Map<String, dynamic> data = const {},
+      Function(dynamic)? onSuccess}) async {
     if (fields.isEmpty) {
       fields = formFields();
     }
@@ -89,25 +95,17 @@ class InvenTreeModel {
       method: "POST",
       fileField: fileField,
     );
-
   }
 
-  Future<void> editForm(BuildContext context, String title, {Map<String, dynamic> fields=const {}, Function(dynamic)? onSuccess}) async {
-
+  Future<void> editForm(BuildContext context, String title,
+      {Map<String, dynamic> fields = const {},
+      Function(dynamic)? onSuccess}) async {
     if (fields.isEmpty) {
       fields = formFields();
     }
 
-    launchApiForm(
-      context,
-      title,
-      url,
-      fields,
-      modelData: jsondata,
-      onSuccess: onSuccess,
-      method: "PATCH"
-    );
-
+    launchApiForm(context, title, url, fields,
+        modelData: jsondata, onSuccess: onSuccess, method: "PATCH");
   }
 
   // JSON data which defines this object
@@ -123,10 +121,8 @@ class InvenTreeModel {
 
   // Construct an InvenTreeModel from a JSON data object
   InvenTreeModel.fromJson(Map<String, dynamic> json) {
-
     // Store the json object
     jsondata = json;
-
   }
 
   int get pk => (jsondata['pk'] ?? -1) as int;
@@ -144,7 +140,6 @@ class InvenTreeModel {
   String get link => jsondata['link'] ?? jsondata['URL'] ?? '';
 
   void goToInvenTreePage() async {
-
     if (await canLaunch(webUrl)) {
       await launch(webUrl);
     } else {
@@ -153,9 +148,7 @@ class InvenTreeModel {
   }
 
   void openLink() async {
-
     if (link.isNotEmpty) {
-
       if (await canLaunch(link)) {
         await launch(link);
       }
@@ -167,39 +160,43 @@ class InvenTreeModel {
   // Create a new object from JSON data (not a constructor!)
   InvenTreeModel createFromJson(Map<String, dynamic> json) {
 
-      var obj = InvenTreeModel.fromJson(json);
+    var obj = InvenTreeModel.fromJson(json);
 
-      return obj;
+    return obj;
   }
 
   // Return the API detail endpoint for this Model object
   String get url => "${URL}/${pk}/".replaceAll("//", "/");
 
   // Search this Model type in the database
-  Future<List<InvenTreeModel>> search(BuildContext context, String searchTerm, {Map<String, String> filters = const {}}) async {
-
+  Future<List<InvenTreeModel>> search(BuildContext context, String searchTerm,
+      {Map<String, String> filters = const {}}) async {
     filters["search"] = searchTerm;
 
     final results = list(filters: filters);
 
     return results;
-
   }
 
-  Map<String, String> defaultListFilters() { return Map<String, String>(); }
+  Map<String, String> defaultListFilters() {
+    return Map<String, String>();
+  }
 
   // A map of "default" headers to use when performing a GET request
-  Map<String, String> defaultGetFilters() { return Map<String, String>(); }
+  Map<String, String> defaultGetFilters() {
+    return Map<String, String>();
+  }
 
   /*
    * Reload this object, by requesting data from the server
    */
   Future<bool> reload() async {
+    var response = await api.get(url,
+        params: defaultGetFilters(), expectedStatusCode: 200);
 
-    var response = await api.get(url, params: defaultGetFilters(), expectedStatusCode: 200);
-
-    if (!response.isValid() || response.data == null || !(response.data is Map)) {
-
+    if (!response.isValid() ||
+        response.data == null ||
+        !(response.data is Map)) {
       // Report error
       if (response.statusCode > 0) {
         await sentryReportMessage(
@@ -211,8 +208,7 @@ class InvenTreeModel {
               "valid": response.isValid().toString(),
               "error": response.error,
               "errorDetail": response.errorDetail,
-            }
-        );
+            });
       }
 
       showServerError(
@@ -221,7 +217,6 @@ class InvenTreeModel {
       );
 
       return false;
-
     }
 
     jsondata = response.data;
@@ -231,18 +226,13 @@ class InvenTreeModel {
 
   // POST data to update the model
   Future<bool> update({Map<String, String> values = const {}}) async {
-
     var url = path.join(URL, pk.toString());
 
     if (!url.endsWith("/")) {
       url += "/";
     }
 
-    var response = await api.patch(
-      url,
-      body: values,
-      expectedStatusCode: 200
-    );
+    var response = await api.patch(url, body: values, expectedStatusCode: 200);
 
     if (!response.isValid()) {
       return false;
@@ -252,8 +242,8 @@ class InvenTreeModel {
   }
 
   // Return the detail view for the associated pk
-  Future<InvenTreeModel?> get(int pk, {Map<String, String> filters = const {}}) async {
-
+  Future<InvenTreeModel?> get(int pk,
+      {Map<String, String> filters = const {}}) async {
     // TODO - Add "timeout"
     // TODO - Add error catching
 
@@ -272,8 +262,9 @@ class InvenTreeModel {
 
     var response = await api.get(url, params: params);
 
-    if (!response.isValid() || response.data == null || !(response.data is Map)) {
-
+    if (!response.isValid() ||
+        response.data == null ||
+        !(response.data is Map)) {
       if (response.statusCode > 0) {
         await sentryReportMessage(
             "InvenTreeModel.get() returned invalid response",
@@ -284,8 +275,7 @@ class InvenTreeModel {
               "valid": response.isValid().toString(),
               "error": response.error,
               "errorDetail": response.errorDetail,
-            }
-        );
+            });
       }
 
       showServerError(
@@ -294,14 +284,12 @@ class InvenTreeModel {
       );
 
       return null;
-
     }
 
     return createFromJson(response.data);
   }
 
   Future<InvenTreeModel?> create(Map<String, dynamic> data) async {
-
     print("CREATE: ${URL} ${data.toString()}");
 
     if (data.containsKey('pk')) {
@@ -315,8 +303,9 @@ class InvenTreeModel {
     var response = await api.post(URL, body: data);
 
     // Invalid response returned from server
-    if (!response.isValid() || response.data == null || !(response.data is Map)) {
-
+    if (!response.isValid() ||
+        response.data == null ||
+        !(response.data is Map)) {
       if (response.statusCode > 0) {
         await sentryReportMessage(
             "InvenTreeModel.create() returned invalid response",
@@ -327,8 +316,7 @@ class InvenTreeModel {
               "valid": response.isValid().toString(),
               "error": response.error,
               "errorDetail": response.errorDetail,
-            }
-        );
+            });
       }
 
       showServerError(
@@ -336,14 +324,14 @@ class InvenTreeModel {
         L10().errorCreate,
       );
 
-
       return null;
     }
 
     return createFromJson(response.data);
   }
 
-  Future<InvenTreePageResponse?> listPaginated(int limit, int offset, {Map<String, String> filters = const {}}) async {
+  Future<InvenTreePageResponse?> listPaginated(int limit, int offset,
+      {Map<String, String> filters = const {}}) async {
     var params = defaultListFilters();
 
     for (String key in filters.keys) {
@@ -362,25 +350,25 @@ class InvenTreeModel {
     // Construct the response
     InvenTreePageResponse page = new InvenTreePageResponse();
 
-    if (response.data.containsKey("count") && response.data.containsKey("results")) {
-       page.count = response.data["count"] as int;
+    if (response.data.containsKey("count") &&
+        response.data.containsKey("results")) {
+      page.count = response.data["count"] as int;
 
-       page.results = [];
+      page.results = [];
 
-       for (var result in response.data["results"]) {
-         page.addResult(createFromJson(result));
-       }
+      for (var result in response.data["results"]) {
+        page.addResult(createFromJson(result));
+      }
 
-       return page;
-
+      return page;
     } else {
       return null;
     }
   }
 
   // Return list of objects from the database, with optional filters
-  Future<List<InvenTreeModel>> list({Map<String, String> filters = const {}}) async {
-
+  Future<List<InvenTreeModel>> list(
+      {Map<String, String> filters = const {}}) async {
     var params = defaultListFilters();
 
     for (String key in filters.keys) {
@@ -407,7 +395,6 @@ class InvenTreeModel {
     }
 
     for (var d in data) {
-
       // Create a new object (of the current class type
       InvenTreeModel obj = createFromJson(d);
 
@@ -416,7 +403,6 @@ class InvenTreeModel {
 
     return results;
   }
-
 
   // Provide a listing of objects at the endpoint
   // TODO - Static function which returns a list of objects (of this class)
@@ -441,7 +427,6 @@ class InvenTreeModel {
   // Each filter must be matched
   // Used for (e.g.) filtering returned results
   bool filter(String filterString) {
-
     List<String> filters = filterString.trim().toLowerCase().split(" ");
 
     for (var f in filters) {
@@ -452,8 +437,9 @@ class InvenTreeModel {
 
     return true;
   }
-}
 
+
+}
 
 class InvenTreeAttachment extends InvenTreeModel {
   // Class representing an "attachment" file
@@ -508,27 +494,19 @@ class InvenTreeAttachment extends InvenTreeModel {
     }
   }
 
-  InvenTreeAttachment.fromJson(Map<String, dynamic> json) : super.fromJson(json);
+  InvenTreeAttachment.fromJson(Map<String, dynamic> json)
+      : super.fromJson(json);
 
-  Future<bool> uploadAttachment(File attachment, {String comment = "", Map<String, String> fields = const {}}) async {
-
+  Future<bool> uploadAttachment(File attachment,
+      {String comment = "", Map<String, String> fields = const {}}) async {
     final APIResponse response = await InvenTreeAPI().uploadFile(
-        URL,
-        attachment,
-        method: 'POST',
-        name: 'attachment',
-        fields: fields
-    );
+        URL, attachment,
+        method: 'POST', name: 'attachment', fields: fields);
 
     return response.successful();
   }
 
   Future<void> downloadAttachment() async {
-
     await InvenTreeAPI().downloadFile(attachment);
-
   }
-
 }
-
-
