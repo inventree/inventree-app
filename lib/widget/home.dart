037564c6a3e976a05e1cb25ef22337d5823f8da8
Oliver <oliver.henry.walters@gmail.com>
2021-09-28 12:19:19 +1000

    Adds workflow for code linting

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -261,25 +261,10 @@ class _InvenTreeHomePageState extends State<InvenTreeHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       key: _homeKey,
       appBar: AppBar(
         title: Text(L10().appTitle),
-        actions: <Widget>[
-          // IconButton(
-          //   icon: FaIcon(FontAwesomeIcons.barcode),
-          //   tooltip: L10().scanBarcode,
-          //   onPressed: () {
-          //     _scan(context);
-          //   },
-          // ),
-        ],
       ),
       drawer: new InvenTreeDrawer(context),
       body: ListView(
