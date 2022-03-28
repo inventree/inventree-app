43faec8b210b2e17d087a9763af1428c5b19e7f4
Oliver Walters <oliver.henry.walters@gmail.com>
2022-03-28 21:23:56 +1100

    Display history

--- lib/widget/stock_item_history.dart
+++ lib/widget/stock_item_history.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:inventree/widget/refreshable_state.dart';
 import 'package:inventree/l10.dart';
 import 'package:inventree/inventree/stock.dart';
+import 'package:inventree/inventree/model.dart';
+
 
 class StockItemHistoryWidget extends StatefulWidget {
 
@@ -27,9 +29,24 @@ class _StockItemHistoryDisplayState extends RefreshableState<StockItemHistoryWid
   @override
   String getAppBarTitle(BuildContext context) => L10().stockItemHistory;
 
+  List<InvenTreeStockItemHistory> history = [];
+
   @override
   Future<void> request(BuildContext refresh) async {
-    // TODO
+
+    history.clear();
+
+    InvenTreeStockItemHistory().list(filters: {"item": "${item.pk}"}).then((List<InvenTreeModel> results) {
+      for (var result in results) {
+        if (result is InvenTreeStockItemHistory) {
+          history.add(result);
+        }
+      }
+
+      // Refresh
+      setState(() {
+      });
+    });
   }
 
   @override
@@ -37,8 +54,25 @@ class _StockItemHistoryDisplayState extends RefreshableState<StockItemHistoryWid
     return ListView(
       children: ListTile.divideTiles(
         context: context,
-        tiles: [],
+        tiles: historyList(),
       ).toList()
     );
+  }
+
+  List<Widget> historyList() {
+    List<Widget> tiles = [];
+
+    for (var entry in history) {
+      tiles.add(
+        ListTile(
+          leading: Text(entry.dateString),
+          trailing: entry.quantityString.isNotEmpty ? Text(entry.quantityString) : null,
+          title: Text(entry.label),
+          subtitle: entry.notes.isNotEmpty ? Text(entry.notes) : null,
+        )
+      );
+    }
+
+    return tiles;
   }
 }
