0c1307dca3a3e74df8ff811d6af2fb7e4f690e61
Oliver Walters <oliver.henry.walters@gmail.com>
2023-04-18 20:28:20 +1000

    Add paginated history widget

--- lib/widget/stock_item_history.dart
+++ lib/widget/stock_item_history.dart
@@ -1,9 +1,12 @@
 import "package:flutter/material.dart";
+import "package:inventree/api.dart";
 
-import "package:inventree/widget/refreshable_state.dart";
 import "package:inventree/l10.dart";
 import "package:inventree/inventree/stock.dart";
 import "package:inventree/inventree/model.dart";
+
+import "package:inventree/widget/paginator.dart";
+import "package:inventree/widget/refreshable_state.dart";
 
 
 class StockItemHistoryWidget extends StatefulWidget {
@@ -69,5 +72,58 @@ class _StockItemHistoryDisplayState extends RefreshableState<StockItemHistoryWid
     }
 
     return tiles;
+  }
+}
+/*
+ * Widget which displays a paginated stock history list
+ */
+class PaginatedStockHistoryList extends PaginatedSearchWidget {
+  const PaginatedStockHistoryList(Map<String, String> filters, bool showSearch)
+      : super(filters: filters, showSearch: showSearch);
+
+  @override
+  _PaginatedStockHistoryState createState() => _PaginatedStockHistoryState();
+}
+
+/*
+ * State class for the paginated stock history list
+ */
+class _PaginatedStockHistoryState extends PaginatedSearchState<PaginatedStockHistoryList> {
+  
+  _PaginatedStockHistoryState() : super();
+
+  @override
+  String get prefix => "stock_history";
+
+  @override
+  Map<String, String> get orderingOptions => {};
+
+  @override
+  Map<String, Map<String, dynamic>> get filterOptions => {
+        // TODO: Add filter options
+      };
+
+  @override
+  Future<InvenTreePageResponse?> requestPage(
+      int limit, int offset, Map<String, String> params) async {
+    await InvenTreeAPI().StockHistoryStatus.load();
+
+    final page = await InvenTreeStockItemHistory()
+        .listPaginated(limit, offset, filters: params);
+
+    return page;
+  }
+
+  @override
+  Widget buildItem(BuildContext context, InvenTreeModel model) {
+    InvenTreeStockItemHistory entry = model as InvenTreeStockItemHistory;
+
+    return ListTile(
+      leading: Text(entry.dateString),
+      trailing:
+          entry.quantityString.isNotEmpty ? Text(entry.quantityString) : null,
+      title: Text(entry.label),
+      subtitle: entry.notes.isNotEmpty ? Text(entry.notes) : null,
+    );
   }
 }
