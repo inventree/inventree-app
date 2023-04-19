e3f5aa6f3a06b9d47a075f1ddf44ac2a90b5472b
Oliver Walters <oliver.henry.walters@gmail.com>
2023-04-19 21:49:27 +1000

    Fix typos

--- lib/api.dart
+++ lib/api.dart
@@ -650,19 +650,20 @@ class InvenTreeAPI {
    * e.g. "part", "change"
    */
   bool checkPermission(String role, String permission) {
+
     // If we do not have enough information, assume permission is allowed
     if (roles.isEmpty) {
-      debug("chekPermission - no roles defined!");
+      debug("checkPermission - no roles defined!");
       return true;
     }
 
     if (!roles.containsKey(role)) {
-      debug("chekPermission - role '$role' not found!");
+      debug("checkPermission - role '$role' not found!");
       return true;
     }
 
     if (roles[role] == null) {
-      debug("chekPermission - role '$role' is null!");
+      debug("checkPermission - role '$role' is null!");
       return true;
     }
 
