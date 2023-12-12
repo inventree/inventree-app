afe561e0027ad382f193a58fde76ffe5830535a2
Oliver Walters <oliver.henry.walters@gmail.com>
2023-12-13 00:18:53 +1100

    Improve barcode handling

--- lib/barcode/sales_order.dart
+++ lib/barcode/sales_order.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
+import "package:inventree/api_form.dart";
 
 import "package:inventree/inventree/part.dart";
 import "package:inventree/inventree/sales_order.dart";
@@ -109,7 +111,69 @@ class SOAllocateStockHandler extends BarcodeHandler {
 
   @override
   Future<void> onBarcodeMatched(Map<String, dynamic> data) async {
-    // TODO
-    return onBarcodeUnknown(data);
+    if (!data.containsKey("line_item")) {
+      return onBarcodeUnknown(data);
+    }
+
+    barcodeSuccessTone();
+    showSnackIcon(L10().allocated, success: true);
+  }
+
+  @override
+  Future<void> onBarcodeUnhandled(Map<String, dynamic> data) async {
+
+    print("onBarcodeUnhandled:");
+    print(data.toString());
+
+    if (!data.containsKey("action_required") || !data.containsKey("line_item")) {
+      return super.onBarcodeUnhandled(data);
+    }
+
+    // Prompt user for extra information to create the allocation
+    var fields = InvenTreeSOLineItem().allocateFormFields();
+
+    // Update fields with data gathered from the API response
+    fields["line_item"]?["value"] = data["line_item"];
+
+    Map<String, dynamic> stock_filters = {
+      "in_stock": true,
+      "available": true,
+    };
+
+    if (data.containsKey("part")) {
+      stock_filters["part"] = data["part"];
+    }
+
+    fields["stock_item"]?["filters"] = stock_filters;
+    fields["stock_item"]?["value"] = data["stock_item"];
+
+    fields["quantity"]?["value"] = data["quantity"];
+
+    fields["shipment"]?["value"] = data["shipment"];
+    fields["shipment"]?["filters"] = {
+      "order": salesOrder!.pk.toString()
+    };
+
+    final context = OneContext().context!;
+
+    launchApiForm(
+      context,
+      L10().allocateStock,
+      salesOrder!.allocate_url,
+    fields,
+    method: "POST",
+    icon: FontAwesomeIcons.rightToBracket,
+    onSuccess: (data) async {
+        showSnackIcon(L10().allocated, success: true);
+    });
+  }
+
+  @override
+  Future<void> onBarcodeUnknown(Map<String, dynamic> data) async {
+    barcodeFailureTone();
+    showSnackIcon(
+        data["error"] as String? ?? L10().barcodeError,
+        success: false
+    );
   }
 }
