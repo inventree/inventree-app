6872bb974701a7283b6a131176bb87af186cf980
Oliver Walters <oliver.henry.walters@gmail.com>
2024-03-06 20:56:57 +1100

    Add default DSN key

---
+++ lib/dsn.dart
@@ -0,0 +1,7 @@
+
+/*
+ * For integration with sentry.io, fill out the SENTRY_DSN_KEY value below.
+ * This should be set to a valid DSN key, from your sentry.io account
+ *
+ */
+String SENTRY_DSN_KEY = "https://fea705aa4b8e4c598dcf9b146b3d1b86@o378676.ingest.sentry.io/5202450";
--- lib/dummy_dsn.dart
+++
@@ -1,3 +0,0 @@
-// Dummy DSN to use for unit testing, etc
-
-const String SENTRY_DSN_KEY = "https://12345678901234567890@abcdef.ingest.sentry.io/11223344";
