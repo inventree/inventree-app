6c9fd28d3fd0768b6ccd14915c43d5912b4f3cb1
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-18 21:48:31 +1000

    Refactor stock barcode operations into new file

--- test/barcode_test.dart
+++ test/barcode_test.dart
@@ -8,8 +8,10 @@
 import "package:flutter_test/flutter_test.dart";
 
 import "package:inventree/api.dart";
-import "package:inventree/barcode/barcode.dart";
 import "package:inventree/helpers.dart";
+
+import "package:inventree/barcode/barcode.dart";
+import "package:inventree/barcode/stock.dart";
 
 import "package:inventree/inventree/part.dart";
 import "package:inventree/inventree/stock.dart";
