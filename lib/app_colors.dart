ccc6d8a98c75eeca182582858286d13bfeca9c02
Oliver Walters <oliver.henry.walters@gmail.com>
2024-09-29 09:57:38 +1000

    Theme adjustments

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,22 +1,23 @@
+import "package:adaptive_theme/adaptive_theme.dart";
 import "package:flutter/material.dart";
 import "package:one_context/one_context.dart";
 
-const Color COLOR_GRAY_LIGHT = Color.fromRGBO(150, 150, 150, 1);
+bool isDarkMode() {
+  BuildContext? context = OneContext().context;
+
+  if (context == null) {
+    return false;
+  }
+
+  return AdaptiveTheme.of(context!).brightness == Brightness.dark;
+}
 
 // Return an "action" color based on the current theme
 Color get COLOR_ACTION {
-
-  // OneContext might not have context, e.g. in testing
-  if (!OneContext.hasContext) {
-    return Colors.lightBlue;
-  }
-
-  BuildContext? context = OneContext().context;
-
-  if (context != null) {
-    return Theme.of(context).indicatorColor;
+  if (isDarkMode()) {
+    return Colors.lightBlueAccent;
   } else {
-    return Colors.lightBlue;
+    return Colors.blue;
   }
 }
 
@@ -24,3 +25,4 @@ const Color COLOR_WARNING = Color.fromRGBO(250, 150, 50, 1);
 const Color COLOR_DANGER = Color.fromRGBO(200, 50, 75, 1);
 const Color COLOR_SUCCESS = Color.fromRGBO(100, 200, 75, 1);
 const Color COLOR_PROGRESS = Color.fromRGBO(50, 100, 200, 1);
+const Color COLOR_GRAY_LIGHT = Color.fromRGBO(150, 150, 150, 1);
