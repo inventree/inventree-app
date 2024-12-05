4e4edb9135c0c9648448eaaf7f59d3343f67d9d9
Oliver Walters <oliver.henry.walters@gmail.com>
2024-12-05 23:47:43 +1100

    Fix AppBar color

--- lib/settings/purchase_order_settings.dart
+++ lib/settings/purchase_order_settings.dart
@@ -1,6 +1,7 @@
 
 import "package:flutter/material.dart";
 import "package:flutter_tabler_icons/flutter_tabler_icons.dart";
+import "package:inventree/app_colors.dart";
 
 import "package:inventree/l10.dart";
 import "package:inventree/preferences.dart";
@@ -39,7 +40,10 @@ class _InvenTreePurchaseOrderSettingsState extends State<InvenTreePurchaseOrderS
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(L10().purchaseOrderSettings)),
+        appBar: AppBar(
+          title: Text(L10().purchaseOrderSettings),
+          backgroundColor: COLOR_APP_BAR,
+        ),
         body: Container(
             child: ListView(
                 children: [
