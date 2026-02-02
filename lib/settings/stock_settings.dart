b40dc0c56dc8ac528dc55ac0e1babded5d245a6a
Oliver Walters <oliver.henry.walters@gmail.com>
2026-02-02 22:20:42 +1100

    Remove unused import

--- lib/settings/stock_settings.dart
+++ lib/settings/stock_settings.dart
@@ -1,4 +1,3 @@
-import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter_tabler_icons/flutter_tabler_icons.dart";
 import "package:inventree/app_colors.dart";
